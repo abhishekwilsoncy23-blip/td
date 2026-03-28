8a82cc6132b8ac63109b6693cf4b53f01deb0794
abhishekwilsoncy23-blip <abhishekwilson.cy23@jecc.ac.in>
2026-03-28 23:19:25 +0530

    Update level1.js

--- level1.js
+++ level1.js
@@ -1,12 +1,24 @@
 window.GAME_LEVELS = window.GAME_LEVELS || [];
 
 window.GAME_LEVELS.push({
-    trackLength: 300,
-    runSpeed: 0.3,
-    fireRate: 20, 
-    obstacles: [
-        { x: -2, z: -50, hp: 5 }, { x: 2, z: -50, hp: 5 },
-        { x: 0, z: -120, hp: 10 },
-        { x: -3, z: -200, hp: 15 }, { x: 3, z: -200, hp: 15 }
-    ]
+    trackLength: 400,
+    runSpeed: 0.35,
+    startAmmo: 20, // Starting bullets
+    gates: [
+        // Choice 1: Left is +15, Right is -10
+        { text: '+15', op: 'add', val: 15, x: -2.5, z: -80, color: 'rgba(0, 255, 0, 0.4)' },
+        { text: '-10', op: 'sub', val: 10, x: 2.5,  z: -80, color: 'rgba(255, 0, 0, 0.4)' },
+        
+        // Choice 2: Left is /2, Right is x2
+        { text: '÷2', op: 'div', val: 2, x: -2.5, z: -180, color: 'rgba(255, 0, 0, 0.4)' },
+        { text: 'x2', op: 'mul', val: 2, x: 2.5,  z: -180, color: 'rgba(0, 255, 0, 0.4)' },
+
+        // Choice 3
+        { text: '+50', op: 'add', val: 50, x: -2.5, z: -280, color: 'rgba(0, 255, 0, 0.4)' },
+        { text: '-50', op: 'sub', val: 50, x: 2.5,  z: -280, color: 'rgba(255, 0, 0, 0.4)' }
+    ],
+    boss: {
+        hp: 120,    // You need 120 bullets to kill him!
+        z: -380     // Placed at the very end
+    }
 });
