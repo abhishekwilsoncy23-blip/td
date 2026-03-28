5325f782bbddca3c845f47a11eddab986ae5f4ee
abhishekwilsoncy23-blip <abhishekwilson.cy23@jecc.ac.in>
2026-03-28 23:13:14 +0530

    Update level1.js

--- level1.js
+++ level1.js
@@ -1,16 +1,12 @@
 window.GAME_LEVELS = window.GAME_LEVELS || [];
 
 window.GAME_LEVELS.push({
-    trackLength: 500,  // How far the soldier runs before winning
-    runSpeed: 0.4,     // Forward speed
-    fireRate: 15,      // Frames between shots
+    trackLength: 300,
+    runSpeed: 0.3,
+    fireRate: 20, 
     obstacles: [
-        // x is left/right (-5 to 5), z is distance forward (negative numbers)
-        { x: -2, z: -50, hp: 5 },
-        { x: 2,  z: -50, hp: 5 },
-        { x: 0,  z: -100, hp: 10 },
-        { x: -3, z: -150, hp: 15 },
-        { x: 3,  z: -150, hp: 15 },
-        { x: 0,  z: -250, hp: 30 } // Boss block
+        { x: -2, z: -50, hp: 5 }, { x: 2, z: -50, hp: 5 },
+        { x: 0, z: -120, hp: 10 },
+        { x: -3, z: -200, hp: 15 }, { x: 3, z: -200, hp: 15 }
     ]
 });
