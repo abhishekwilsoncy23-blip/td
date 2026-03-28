ed4b0cf10160ac3a95aefc92a26f0dc6fe784724
abhishekwilsoncy23-blip <abhishekwilson.cy23@jecc.ac.in>
2026-03-28 23:26:46 +0530

    Create level2.js

---
+++ level2.js
@@ -0,0 +1,22 @@
+window.GAME_LEVELS = window.GAME_LEVELS || [];
+
+window.GAME_LEVELS.push({
+    trackLength: 600,
+    runSpeed: 0.35,
+    startAmmo: 30,
+    gates: [
+        { text: '+20', op: 'add', val: 20, x: -2.5, z: -80 },
+        { text: '-10', op: 'sub', val: 10, x: 2.5,  z: -80 },
+        { text: 'x2',  op: 'mul', val: 2,  x: -2.5, z: -180 },
+        { text: '÷2',  op: 'div', val: 2,  x: 2.5,  z: -180 },
+        { text: '+40', op: 'add', val: 40, x: -2.5, z: -350 },
+        { text: '-30', op: 'sub', val: 30, x: 2.5,  z: -350 }
+    ],
+    enemies: [
+        { hp: 30, x: 0, z: -250 } 
+    ],
+    boss: {
+        hp: 180,
+        z: -550
+    }
+});
