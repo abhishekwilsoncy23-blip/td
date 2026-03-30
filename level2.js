3ac6a29678b80aae63920ecb9146afd1b1a0ed1b
abhishekwilsoncy23-blip <abhishekwilson.cy23@jecc.ac.in>
2026-03-30 09:48:29 +0530

    Update level2.js

--- level2.js
+++ level2.js
@@ -5,11 +5,11 @@ window.GAME_LEVELS.push({
     runSpeed: 0.35,
     startAmmo: 30,
     gates: [
-        { text: '+20', op: 'add', val: 20, x: -2.5, z: -80 },
+        { text: '+20', op: 'add', val: 40, x: -2.5, z: -80 },
         { text: '-10', op: 'sub', val: 10, x: 2.5,  z: -80 },
         { text: 'x2',  op: 'mul', val: 2,  x: -2.5, z: -180 },
         { text: '÷2',  op: 'div', val: 2,  x: 2.5,  z: -180 },
-        { text: '+40', op: 'add', val: 40, x: -2.5, z: -350 },
+        { text: '+40', op: 'add', val: 70, x: -2.5, z: -350 },
         { text: '-30', op: 'sub', val: 30, x: 2.5,  z: -350 }
     ],
     enemies: [
