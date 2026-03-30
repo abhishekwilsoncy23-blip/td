1af09e2fc4c7dfc9ecef03e26022e1a30d75e57a
abhishekwilsoncy23-blip <abhishekwilson.cy23@jecc.ac.in>
2026-03-30 09:41:40 +0530

    Adjust track length and boss position in level3

--- level3.js
+++ level3.js
@@ -1,8 +1,8 @@
 window.GAME_LEVELS = window.GAME_LEVELS || [];
 
 window.GAME_LEVELS.push({
-    trackLength: 800,
-    runSpeed: 0.4, // Runs slightly faster!
+    trackLength: 1000,
+    runSpeed: 0.4,
     startAmmo: 40,
     gates: [
         { text: '+30', op: 'add', val: 30, x: -2.5, z: -100 },
@@ -13,15 +13,14 @@ window.GAME_LEVELS.push({
         { text: '÷3',  op: 'div', val: 3,  x: 2.5,  z: -550 }
     ],
     enemies: [
-        // Two guards side-by-side forcing you to pick one to kill!
         { hp: 40, x: -2.5, z: -400 },
         { hp: 40, x: 2.5, z: -400 }
     ],
     mines: [
-        { x: 0, z: -200 },    // Mine in the middle
-        { x: -3, z: -480 },   // Mine on the left
-        { x: 3, z: -480 },    // Mine on the right
-        { x: 0, z: -650 }     // Mine right before the boss!
+        { x: 0, z: -200 },    
+        { x: -3, z: -480 },   
+        { x: 3, z: -480 },    
+        { x: 0, z: -650 }     
     ],
-    boss: { hp: 250, z: -750 }
+    boss: { hp: 250, z: -900 }
 });
